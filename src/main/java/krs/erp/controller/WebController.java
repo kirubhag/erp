79c9184919cdac39a67e6a35231bddae18a5a649
kirubhag <kirubhag@gmail.com>
2025-10-21 13:16:40 +0530

    Fix navigation menu routing issues and welcome page
    - Remove conflicting controller mapping for root path
    - Copy index.html to root static folder for Spring Boot welcome page
    - Remove Thymeleaf template conflicts with static HTML SPA
    - Navigation menu now works correctly with proper AngularJS routing
    - URLs show correct format (#!/students) instead of double hash (#!/#%2Fstudents)
    
    Resolves: Menu navigation not working due to routing conflicts

--- src/main/java/krs/erp/controller/WebController.java
+++ src/main/java/krs/erp/controller/WebController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+    // Removed index mapping to allow static index.html to be served
+    // The static index.html in resources/static/templates/ will be served for the root path
     
     @GetMapping("/login")
     public String login() {
