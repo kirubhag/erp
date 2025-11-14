ee0a6ebf52767ce80eb757f0bf860990009d3fb8
kirubhag <kirubhag@gmail.com>
2025-11-14 17:07:13 +0530

    Fix User entity JSON serialization - add @JsonIgnore to roles and relationships to prevent lazy loading errors

--- src/main/java/krs/erp/model/User.java
+++ src/main/java/krs/erp/model/User.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -73,6 +75,7 @@ public class User extends BaseEntity {
     private LocalDateTime passwordChangeDate;
     
     // Relationships
+    @JsonIgnore
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "user_roles",
@@ -81,12 +84,15 @@ public class User extends BaseEntity {
     )
     private Set<Role> roles = new HashSet<>();
     
+    @JsonIgnore
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
     private Student student;
     
+    @JsonIgnore
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
     private Staff staff;
     
+    @JsonIgnore
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
     private Parent parent;
     
