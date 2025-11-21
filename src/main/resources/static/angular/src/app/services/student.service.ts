ca33e4bae97bf13df9573cb36edf13cde335444f
kirubhag <kirubhag@gmail.com>
2025-11-21 09:17:40 +0530

    Fix student service API endpoint back to /api/students to match backend controller

--- src/main/resources/static/angular/src/app/services/student.service.ts
+++ src/main/resources/static/angular/src/app/services/student.service.ts
@@ -7,7 +7,7 @@ import { Student } from '../components/student-list/student-list.component';
   providedIn: 'root'
 })
 export class StudentService {
-  private baseUrl = '/settings/students';
+  private baseUrl = '/api/students';
 
   constructor(private http: HttpClient) {}
 
